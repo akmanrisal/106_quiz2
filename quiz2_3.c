f0528b706dbd50ed8d67553e525b2e646447fbc2
akmanrisal <akmanrisal.r@ku.th>
2021-03-25 18:10:33 +0700

    test

---
+++ quiz2_3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int j=1; j<=n; j++)
+    {
+        if(j<=10)
+        {
+            for(int i=1; i<n; i++)
+            {
+                if(i%10==i)
+                    sum++;
+            }
+        }
+        if(j<=100)
+        {
+            for(int i=1; i<n; i++)
+            {
+                if(i%10!=0)
+                {
+                    if(i%11==0)
+                        sum++;
+                }
+            }break;
+        }
+    }
+    printf("%d",sum);
+}
